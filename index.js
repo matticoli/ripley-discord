4c2f451b3675018dd01fd40091e2af24099788b7
Mikel Matticoli <mikel.matticoli@gmail.com>
2020-09-29 13:22:47 -0400

    Bork

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+    if (msg.content.includes('Ripley')) {
+      msg.reply('Bork bork!');
+    }
+  });
