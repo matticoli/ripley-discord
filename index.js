1b477cb99f279182731c0ace0eb66e107dd11565
Mikel Matticoli <mikel.matticoli@gmail.com>
2020-09-30 14:05:10 -0400

    fetch

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const Discord = require('discord.js');
+const axios = require('axios');
 const bot = new Discord.Client();
 
 const TOKEN = process.env.TOKEN;
+const KEY = process.env.KEY;
 
 bot.login(TOKEN);
 
@@ -11,7 +13,20 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-    if (msg.content.includes('Ripley')) {
+    let text = msg.content.toLowerCase();
+    if (text.includes('ripley') || text.includes('@ripley')) {
       msg.reply('Bork bork!');
+    } else if (text.includes('fetch')) {
+
+      axios.get(`https://sheets.googleapis.com/v4/spreadsheets/1cWbL5jW5QyGfssRjV1GRVd5LDTEvQ178wLHpSAhhoz0/values/Sheet1!A:A?key=${KEY}`).then(resp => {
+              const photos = resp.data.values;
+
+              let imgLink = photos[Math.floor(Math.random() * photos.length)][0];
+              let embed = new Discord.MessageEmbed().setImage(imgLink).setURL(imgLink);
+
+              msg.channel.send(embed);
+      }).catch(err => {
+        msg.channel.send("Bork bork! Nah. Blame Google");
+      });
     }
   });
